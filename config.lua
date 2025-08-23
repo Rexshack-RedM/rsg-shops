62d80a7e40c9fc70848f2eaf83db5a319eb7b436
I AM PERE <75316668+iampere4u@users.noreply.github.com>
2025-08-23 18:48:09 +0530

    Update config.lua
    Shop open and close hour added differently for different locations with the blips Input 
    Menu added for buying items 
    Pricemultiplier added so the different stores will have different pricing 
    Everything is configurable in config.lua

--- config.lua
+++ config.lua
@@ -4,49 +4,42 @@ Config = {}
 -- settings
 ---------------------------------
 Config.Keybind = 'J' -- keybind prompt to open shop
-
 ---------------------------------
--- shop items
-
--- amount: default stock (remove to enable unlimited stock)
--- buyPrice: enables selling items to shop
--- maxStock: limits how much items players can sell to shop
--- minQuality: minimum quality that can be sold to shop (default 1)
--- restock: amount of items to be restocked per restock cycle
-
--- example: { name = 'bread', amount = 50, price = 0.10, buyPrice = 0.05, maxStock = 100, minQuality = 50, restock = 10 },
+-- shop products
 ---------------------------------
 Config.Products = {
-    ['normal'] = {
-        { name = 'bread', amount = 50, price = 0.10 },
-        { name = 'water', amount = 50, price = 0.10 },
+     ['normal'] = {
+        food = {
+            { name = 'bread', amount = 50, price = 0.10 },
+            { name = 'meat', amount = 20, price = 0.50 },
+        },
+        drinks = {
+            { name = 'water', amount = 50, price = 0.10 },
+            { name = 'whiskey', amount = 10, price = 2.0 },
+        },
+        others = {
+            { name = 'bandage', amount = 10, price = 1.5 },
+            { name = 'torch', amount = 5, price = 3.0 },
+        }
     },
     ['weapons'] = {
-        { name = 'weapon_revolver_cattleman',            amount = 1, price = 50 },
-        { name = 'weapon_revolver_doubleaction',         amount = 1, price = 127 },
-        { name = 'weapon_revolver_doubleaction_gambler', amount = 1, price = 190 },
-        { name = 'weapon_revolver_lemat',                amount = 1, price = 317 },
-        { name = 'weapon_revolver_navy',                 amount = 1, price = 275 },
-        { name = 'weapon_revolver_schofield',            amount = 1, price = 192 },
-        { name = 'weapon_pistol_mauser',                 amount = 1, price = 600 },
-        { name = 'weapon_pistol_semiauto',               amount = 1, price = 537 },
-        { name = 'weapon_pistol_volcanic',               amount = 1, price = 270 },
-        { name = 'weapon_rifle_boltaction',              amount = 1, price = 216 },
-        { name = 'weapon_rifle_elephant',                amount = 1, price = 580 },
-        { name = 'weapon_rifle_springfield',             amount = 1, price = 156 },
-        { name = 'weapon_rifle_varmint',                 amount = 1, price = 72 },
-        { name = 'weapon_repeater_carbine',              amount = 1, price = 90 },
-        { name = 'weapon_repeater_evans',                amount = 1, price = 300 },
-        { name = 'weapon_repeater_winchester',           amount = 1, price = 243 },
-        { name = 'weapon_repeater_henry',                amount = 1, price = 348 },
-        { name = 'weapon_sniperrifle_rollingblock',      amount = 1, price = 411 },
-        { name = 'weapon_sniperrifle_carcano',           amount = 1, price = 456 },
-        { name = 'ammo_box_revolver',                    amount = 10, price = 10 },
-        { name = 'ammo_box_pistol',                      amount = 10, price = 10 },
-        { name = 'ammo_box_rifle',                       amount = 10, price = 10 },
-        { name = 'ammo_box_repeater',                    amount = 10, price = 10 },
-        { name = 'ammo_box_shotgun',                     amount = 10, price = 10 },
-        { name = 'ammo_box_rifle_elephant',              amount = 10, price = 10 },
+        weapons = {
+            { name = 'weapon_revolver_cattleman',  amount = 1, price = 50 },
+            { name = 'weapon_revolver_doubleaction', amount = 1, price = 127 },
+            { name = 'weapon_revolver_lemat', amount = 1, price = 317 },
+            { name = 'weapon_revolver_navy', amount = 1, price = 275 },
+            { name = 'weapon_pistol_mauser', amount = 1, price = 600 },
+            { name = 'weapon_rifle_boltaction', amount = 1, price = 216 },
+            { name = 'weapon_repeater_winchester', amount = 1, price = 243 },
+            { name = 'weapon_sniperrifle_rollingblock', amount = 1, price = 411 },
+        },
+        ammo = {
+            { name = 'ammo_box_revolver', amount = 10, price = 10 },
+            { name = 'ammo_box_pistol', amount = 10, price = 10 },
+            { name = 'ammo_box_rifle', amount = 10, price = 10 },
+            { name = 'ammo_box_repeater', amount = 10, price = 10 },
+            { name = 'ammo_box_shotgun', amount = 10, price = 10 },
+        }
     },
     ['armoury'] = {
         { name = 'weapon_revolver_cattleman',  amount = 1, price = 0 },
@@ -82,6 +75,10 @@ Config.StoreLocations = {
         blipscale = 0.2,
         showblip = true,
         persistentStock = false,
+        useHours = true,
+        openHour = 9,
+        closeHour = 21,
+        priceMultiplier = 10, 
     },
     {
         label = 'Valentine General Store',
@@ -92,6 +89,10 @@ Config.StoreLocations = {
         blipscale = 0.2,
         showblip = true,
         persistentStock = false,
+        useHours = true,
+        openHour = 8,
+        closeHour = 22,
+        priceMultiplier = 20, 
     },
     {
         label = 'Strawberry General Store',
@@ -102,6 +103,10 @@ Config.StoreLocations = {
         blipscale = 0.2,
         showblip = true,
         persistentStock = false,
+        useHours = true,
+        openHour = 10,
+        closeHour = 18,
+        priceMultiplier = 1, --- for the same price 
     },
     {
         label = 'Annesburg General Store',
@@ -112,6 +117,10 @@ Config.StoreLocations = {
         blipscale = 0.2,
         showblip = true,
         persistentStock = false,
+        useHours = true,
+        openHour = 9,
+        closeHour = 20,
+        priceMultiplier = 1, --- for the same price 
     },
     {
         label = 'Saint Denis General Store',
@@ -122,6 +131,10 @@ Config.StoreLocations = {
         blipscale = 0.2,
         showblip = true,
         persistentStock = false,
+        useHours = true,
+        openHour = 7,
+        closeHour = 23,
+        priceMultiplier = 1, --- for the same price 
     },
     {
         label = 'Tumbleweed General Store',
@@ -132,6 +145,10 @@ Config.StoreLocations = {
         blipscale = 0.2,
         showblip = true,
         persistentStock = false,
+        useHours = true,
+        openHour = 8,
+        closeHour = 22,
+        priceMultiplier = 1, --- for the same price 
     },
     {
         label = 'Armadillo General Store',
@@ -142,6 +159,10 @@ Config.StoreLocations = {
         blipscale = 0.2,
         showblip = true,
         persistentStock = false,
+        useHours = true,
+        openHour = 10,
+        closeHour = 20,
+        priceMultiplier = 1, --- for the same price 
     },
     {
         label = 'Blackwater General Store',
@@ -152,6 +173,10 @@ Config.StoreLocations = {
         blipscale = 0.2,
         showblip = true,
         persistentStock = false,
+        useHours = true,
+        openHour = 9,
+        closeHour = 21,
+        priceMultiplier = 1, --- for the same price 
     },
     {
         label = 'Van Horn General Store',
@@ -161,60 +186,82 @@ Config.StoreLocations = {
         blipsprite = 'blip_shop_store',
         blipscale = 0.2,
         showblip = true,
-        persistentStock = false,
+        persistentStock = true,
+        useHours = false, -- 24/7 outlaw town
+        priceMultiplier = 1,
     },
+
     ---------------------------------
     -- gunsmith
     ---------------------------------
     {
-        label = 'Valentine Gunsmith',
-        name = 'wep-valentine',
-        products = 'weapons',
-        shopcoords = vector3(-281.24, 780.71, 119.53),
-        blipsprite = 'blip_shop_gunsmith',
-        blipscale = 0.2,
-        showblip = true,
-        persistentStock = false,
-    },
-    {
-        label = 'Tumbleweed Gunsmith',
-        name = 'wep-tumbleweed',
-        products = 'weapons',
-        shopcoords = vector3(-5508.18, -2964.27, -0.63),
-        blipsprite = 'blip_shop_gunsmith',
-        blipscale = 0.2,
-        showblip = true,
-        persistentStock = false,
-    },
-    {
-        label = 'Saint Denis Gunsmith',
-        name = 'wep-stdenis',
-        products = 'weapons',
-        shopcoords = vector3(2716.42, -1285.42, 49.63),
-        blipsprite = 'blip_shop_gunsmith',
-        blipscale = 0.2,
-        showblip = true,
-        persistentStock = false,
-    },
-    {
-        label = 'Rhodes Gunsmith',
-        name = 'wep-rhodes',
-        products = 'weapons',
-        shopcoords = vector3(1322.75, -1321.47, 77.89),
-        blipsprite = 'blip_shop_gunsmith',
-        blipscale = 0.2,
-        showblip = true,
-        persistentStock = false,
-    },
-    {
-        label = 'Annesburg Gunsmith',
-        name = 'wep-annesburg',
-        products = 'weapons',
-        shopcoords = vector3(2946.50, 1319.53, 44.82),
-        blipsprite = 'blip_shop_gunsmith',
-        blipscale = 0.2,
-        showblip = true,
-        persistentStock = false,
-    },
-
+    label = 'Valentine Gunsmith',
+    name = 'wep-valentine',
+    products = 'weapons',
+    shopcoords = vector3(-281.24, 780.71, 119.53),
+    blipsprite = 'blip_shop_gunsmith',
+    blipscale = 0.2,
+    showblip = true,
+    persistentStock = false,
+    useHours = true,
+    openHour = 10,
+    closeHour = 20,
+    priceMultiplier = 5,  
+},
+{
+    label = 'Tumbleweed Gunsmith',
+    name = 'wep-tumbleweed',
+    products = 'weapons',
+    shopcoords = vector3(-5508.18, -2964.27, -0.63),
+    blipsprite = 'blip_shop_gunsmith',
+    blipscale = 0.2,
+    showblip = true,
+    persistentStock = false,
+    useHours = true,
+    openHour = 9,
+    closeHour = 19,
+    priceMultiplier = 2,  
+},
+{
+    label = 'Saint Denis Gunsmith',
+    name = 'wep-stdenis',
+    products = 'weapons',
+    shopcoords = vector3(2716.42, -1285.42, 49.63),
+    blipsprite = 'blip_shop_gunsmith',
+    blipscale = 0.2,
+    showblip = true,
+    persistentStock = false,
+    useHours = true,
+    openHour = 8,
+    closeHour = 22,
+    priceMultiplier = 3,  
+},
+{
+    label = 'Rhodes Gunsmith',
+    name = 'wep-rhodes',
+    products = 'weapons',
+    shopcoords = vector3(1322.75, -1321.47, 77.89),
+    blipsprite = 'blip_shop_gunsmith',
+    blipscale = 0.2,
+    showblip = true,
+    persistentStock = false,
+    useHours = true,
+    openHour = 10,
+    closeHour = 20,
+    priceMultiplier = 1,  
+},
+{
+    label = 'Annesburg Gunsmith',
+    name = 'wep-annesburg',
+    products = 'weapons',
+    shopcoords = vector3(2946.50, 1319.53, 44.82),
+    blipsprite = 'blip_shop_gunsmith',
+    blipscale = 0.2,
+    showblip = true,
+    persistentStock = false,
+    useHours = true,
+    openHour = 9,
+    closeHour = 19,
+    priceMultiplier = 4,  
+},
 }
